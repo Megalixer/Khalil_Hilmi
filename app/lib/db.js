3c2c834d370727687bc73b194de161368cc91151
Megalixer <hilmir0409@gmail.com>
2026-06-07 21:38:53 +0700

    modified:   app/lib/db.js

--- app/lib/db.js
+++ app/lib/db.js
@@ -1,15 +1,11 @@
-import mysql from 'mysql2/promise';
+import mysql from "mysql2/promise";
 
-let connection;
-export const createConnection = async () => {
-    if(!connection) {
-        connection = await mysql.createConnection({
-            host: process.env.DATABASE_HOST,
-            port: Number(process.env.DATABASE_PORT),
-            user: process.env.DATABASE_USER,
-            password: process.env.DATABASE_PASSWORD,
-            database: process.env.DATABASE_NAME
-        });
-    }
-    return connection;
-}
+connection = await mysql.createConnection({
+  host: process.env.DATABASE_HOST,
+  port: Number(process.env.DATABASE_PORT),
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  ssl: {}
+});
+export default connection;
